3197aba5696750008e8b9ff0867da70f4f2201a7
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-10 12:58:42 +0100

    Add project files.

---
+++ FunzioneCheConverteInAscii/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FunzioneCheConverteInAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = 'D';
+            int value = Convert.ToInt32(input);
+            Console.WriteLine($"decimal value of {input} is {value}");
+            string hexOutput = String.Format("{0:X}", value);
+            Console.WriteLine($"Hexadecimal value of {input} is {hexOutput}");
+            string binary= Convert.ToString(input,2);
+            Console.WriteLine($"binary value of {input} is {binary}");
+        }
+    }
+}
