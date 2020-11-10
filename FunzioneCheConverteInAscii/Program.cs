5f4598b050f2e76f34fbf96c7136077a2ff1f9a3
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-10 13:06:36 +0100

    modifica

--- FunzioneCheConverteInAscii/Program.cs
+++ FunzioneCheConverteInAscii/Program.cs
@@ -6,12 +6,15 @@ namespace FunzioneCheConverteInAscii
     {
         static void Main(string[] args)
         {
-            char input = 'D';
+            Console.Write("inserisci un carattere: ");
+            char input = char.Parse(Console.ReadLine());
             int value = Convert.ToInt32(input);
             Console.WriteLine($"decimal value of {input} is {value}");
             string hexOutput = String.Format("{0:X}", value);
             Console.WriteLine($"Hexadecimal value of {input} is {hexOutput}");
-            string binary= Convert.ToString(input,2);
+            string octave= Convert.ToString(input,8);
+            Console.WriteLine($"octave value of {input} is {octave}");
+            string binary = Convert.ToString(input, 2);
             Console.WriteLine($"binary value of {input} is {binary}");
         }
     }
